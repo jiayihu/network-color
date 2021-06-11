67a107be8be650276dfdbbb834519ada3e6324bc
Jiayi Hu <jiayi.ghu@gmail.com>
2021-06-12 01:09:53 +0200

    Render PixelImage with canvas

--- src/components/PixelImage.tsx
+++ src/components/PixelImage.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+import chroma from 'chroma-js';
 import { css } from '@emotion/css';
 
 export type Props = {
@@ -7,39 +9,45 @@ export type Props = {
   activeColors: string[];
 };
 
+function setCanvasPixel(rgba: Uint8ClampedArray, width: number, i: number, [r, g, b]: number[]) {
+  rgba[i * 4] = r;
+  rgba[i * 4 + 1] = g;
+  rgba[i * 4 + 2] = b;
+  rgba[i * 4 + 3] = 255;
+}
+
 export function PixelImage(props: Props) {
   const { pixels, width, height, activeColors } = props;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const canvas = canvasRef.current;
+    canvas.width = width;
+    canvas.height = height;
+
+    const ctx = canvas.getContext('2d')!;
+    const rgba = pixels.reduce((rgba, pixel, i) => {
+      const isActive = activeColors.includes(pixel);
+
+      if (isActive) {
+        setCanvasPixel(rgba, width, i, chroma(pixel).rgb());
+      } else {
+        setCanvasPixel(rgba, width, i, [255, 255, 255]);
+      }
+
+      return rgba;
+    }, new Uint8ClampedArray(width * height * 4));
+
+    const imageData = new ImageData(rgba, width, height);
+    console.log(imageData);
+    ctx.putImageData(imageData, 0, 0);
+  }, [pixels, activeColors, width, height]);
 
   return (
-    <div
-      className={css(imgStyle, {
-        gridTemplateColumns: `repeat(${width}, 3px)`,
-        gridTemplateRows: `repeat(${height}, 3px)`,
-      })}
-    >
-      {Array.from({ length: width * height }, (_, i) => i).map((i) => {
-        const color = pixels[i];
-        const isActive = activeColors.includes(color);
-
-        return (
-          <span
-            className={css(dotStyle, {
-              backgroundColor: isActive ? color : 'transparent',
-            })}
-            key={i}
-          ></span>
-        );
-      })}
+    <div className={css({ width: width * 2, height: height * 2 })}>
+      <canvas ref={canvasRef} className={css({ width: width * 2, height: height * 2 })} />
     </div>
   );
 }
-
-const imgStyle = css({
-  border: '1px dashed #ccc',
-  display: 'grid',
-  gap: '1px',
-});
-
-const dotStyle = css({
-  borderRadius: '50%',
-});
